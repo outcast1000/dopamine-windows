06faeea0d4d8a73c1ac0f0f16fb0bccb7fdd1a1c
outcast1000 <a.economou@gmail.com>
2020-11-21 01:00:05 +0200

    Playlist: Implement Locate track on playlist + Search #7 In this commit it was the press the NowPlaying Item Info (Left Down) (change) minor ui details

--- Dopamine/ViewModels/Common/CollectionPlaybackControlsViewModel.cs
+++ Dopamine/ViewModels/Common/CollectionPlaybackControlsViewModel.cs
@@ -1,11 +1,18 @@
-﻿using Dopamine.Services.Dialog;
+﻿using Dopamine.Core.Prism;
+using Dopamine.Services.Dialog;
+using Dopamine.Services.Entities;
+using Dopamine.Services.Playback;
 using Dopamine.ViewModels.Common.Base;
+using Prism.Commands;
+using Prism.Events;
 using Prism.Ioc;
 
 namespace Dopamine.ViewModels.Common
 {
     public class CollectionPlaybackControlsViewModel : PlaybackControlsViewModelBase
     {
+        public DelegateCommand LocateTrack { get; set; }
+        private IContainerProvider container;
 
         public bool IsPlaying
         {
@@ -14,6 +21,13 @@ namespace Dopamine.ViewModels.Common
 
         public CollectionPlaybackControlsViewModel(IContainerProvider container, IDialogService dialogService) : base(container)
         {
+            this.container = container;
+            this.LocateTrack = new DelegateCommand(() =>
+            {
+                TrackViewModel vm = container.Resolve<IPlaybackService>().CurrentTrack;
+                if (vm != null)
+                    container.Resolve<IEventAggregator>().GetEvent<LocateItem<TrackViewModel>>().Publish(vm);
+            });
         }
 
         protected override void OnLoad()
